f664cce4f1207d7e1aba03432b4836dab3fd93c2
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-15 17:16:32 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	printf("Size of char: %lu bytes\n", sizeof(char));
-	printf("Size of int: %lu bytes\n", sizeof(int));
-	printf("Size of long: %lu bytes\n", sizeof(long));
-	printf("Size of long long: %lu bytes\n", sizeof(long long));
-	printf("Size of float: %lu bytes\n", sizeof(float));
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of long: %d byte(s)\n", sizeof(long int));
+	printf("Size of long long: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
