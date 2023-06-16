02437cef88bd8e3972f3bae2961e483f6063fa3c
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-16 01:34:39 -0700

    0-preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ long int c;
 long long int d;
 float f;
 
-printf("Size of char: %lu byte(s)\n"), (unsigned long)sizeof(a);
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of int: %lu  byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long int : %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
