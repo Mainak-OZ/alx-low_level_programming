0e73b92a743ee9bd8d3b75b6899b8c2c8766afde
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-16 02:02:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - C program that prints exact line
- * Return 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
