967ab7a7041899bf5ccd9761e9122051d7243e84
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-16 05:28:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,20 @@
 int main() 
 {
 	int n;
-
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    
-    if (n > 0) 
-    {
-        printf("The number %d is positive.\n", n);
-    } else if (n < 0) {
-        printf("The number %d is negative.\n", n);
-    } else {
-        printf("The number is zero.\n");
-    }
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		printf("The number %d is positive.\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("The number %d is negative.\n", n);
+	}
+	else
+	{
+		printf("The number is zero.\n");
+	}
+	return 0;
 }
