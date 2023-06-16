e5716caae9960c8df5523019e3214fe4e2613ba2
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-15 18:24:28 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+char a;
+int b;
+long int c;
+long long int d;
+float f;
 
 printf("Size of char: %lu byte(s)\n"), (unsigned long)size of(a));
 printf("Size of int: %lu  byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of long: %lu byte(s)\n",(unsigned long) sizeof(c));
 printf("Size of long long: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %d byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
