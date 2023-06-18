4bf0c26927eda95a82be112228e83f97c2032f6c
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-18 15:56:54 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,17 +4,17 @@
  * main - prints all possible combinations of single-digit numbers
  * Return: 0 (Success)
  */
-int main()
+int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 48; num < 58; num++)
-    putchar(num);
-    if (num != 57)
-    {
-        putchar(',');
-	putchar('num ');
-    }
-    putchar('\n');
-    return (0);
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	if (num != 57)
+	{
+		putchar(',');
+		putchar('num');
+	}
+	putchar('\n');
+	return (0);
 }
