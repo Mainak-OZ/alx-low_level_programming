7f09235ae4db9c75becf0c0b748a4a0afcbe23f3
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-18 16:10:29 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,13 @@ int main(void)
 	int num;
 
 	for (num = 48; num < 58; num++)
-		putchar(num);
-	if (num != 57)
 	{
-		putchar(', ');
-		putchar(' ');
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
