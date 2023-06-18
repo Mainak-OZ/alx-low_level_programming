ce1bda241188544632b085f6c4b704fc4180534e
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-18 16:20:45 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
 
 int main()
 {
-        int n, m, l;
+	int n, m, l;
 
-        for (n = 48; n < 58; n++)
-        {
-                for (m = 49; m < 58; m++)
-                {
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
 			for (l = 50; l < 58; l++)
-			{	
-                        	if (l > m  && m > n)
+			{
+				if (l > m  && m > n)
 				{
 					putchar(n);
-                              		putchar(m);
+					putchar(m);
 					putchar(l);
-                               		if (n != 55 || m != 56)
+					if (n != 55 || m != 56)
 					{
-                                        putchar(',');
-					putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
 		}
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
