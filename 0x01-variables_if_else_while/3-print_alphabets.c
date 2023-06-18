e95868d3dd2a3df8a93022ebfca3b34e107d2921
Mainak-OZ <mk.martin0416@gmail.com>
2023-06-18 15:10:00 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,23 @@
 
 int main(void)
 {
-        char c;
+	char c;
 	char d;
-        
-        c = 'a';
+	
+	c = 'a';
 	d = 'A';
-        while
-                (c <= 'z') {
-                        putchar(c);
-                        c++;
-                }
 	while
-                (d <= 'Z') {
-			putchar(d);
-                        d++;
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
 		}
-        putchar('\n');
-        return (0);
-}  
+	while
+		(d <= 'Z')
+		 {
+			putchar(d);
+			d++;
+		 }
+	putchar('\n');
+	return (0);
+}
