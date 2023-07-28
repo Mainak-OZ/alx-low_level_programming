142d981a79b7752b47dcb73622b09e2942f3c3ad
Mainak-OZ <mk.martin0416@gmail.com>
2023-07-28 01:00:37 -0700

    Revise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function executed
+ * first - prints a sentence before execution of  main
+ * Return: function executed
  */
 
 void first(void)
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,19 +2,18 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list_t lis
+ * free_list - frees a list_t list
  * @head: list_t list to free
  */
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp = head;
 
 	while (head)
 	{
 		temp = head->next;
 		free(head->str);
 		free(head);
-		head = temp;
 	}
 }
