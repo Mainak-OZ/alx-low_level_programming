d94831ac50d287639effddd46a622603a818613f
Mainak-OZ <mk.martin0416@gmail.com>
2023-07-28 00:32:20 -0700

    Revise

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,13 @@
-	global   main
-	  extern    printf
+global main
+extern printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
-	  ret
-format: db `Hello, Holberton\n`,0
+	mov edi, format_str
+	xor eax, eax
+
+	call printf
+
+	mov exit, 0
+	ret edi, edi
+section .data
+string_format: db `Hello, Holberton\n`,0
