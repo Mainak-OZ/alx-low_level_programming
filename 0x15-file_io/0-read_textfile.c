1336f91ee895c14f59469c04f36fef77681eed32
Mainak-OZ <mk.martin0416@gmail.com>
2023-08-08 04:50:37 -0700

    C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- reads and prints a text file to the POSIX standard output
+ * @filename: text file being read
+ * @letters: number of letters to read and print
+ * Return: r- actual number of bytes read and printed
+ * Return 0 when filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buff;
+	ssize_t fd;
+	ssize_t r;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	t = read(fd, buff, letters);
+	r = write(STDOUT_FILENO, buf, t);
+
+	free(buff);
+	close(fd);
+	return (r);
+}
